323f8a940c87b5cde53132647744b21da5123995
HaoHuynh <hao152903@gmail.com>
2020-10-11 21:33:57 +0700

    11/10/2020-Update 02

---
+++ load_datacsv.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import cv2
+import numpy as np
+
+image_size=(90, 90)
+dataset_path=''
+
+def load_data(dataset_path):
+    data = pd.read_csv(dataset_path)
+    pixels = data['pixels'].tolist()
+    width, height = 90, 90
+    faces = []
+    for pixel_sequence in pixels:
+        face = [int(pixel) for pixel in pixel_sequence.split(' ')]
+        face = np.asarray(face).reshape(width, height)
+        face = cv2.resize(face.astype('uint8'),image_size)
+        faces.append(face.astype('float32'))
+    faces = np.asarray(faces)
+    faces = np.expand_dims(faces, -1)
+    emotions = pd.get_dummies(data['emotion']).to_numpy()
+    return faces, emotions
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
